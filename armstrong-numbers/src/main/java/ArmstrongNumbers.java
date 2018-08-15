32837a4ebdd9ac9b8bb4e9d566a8565331c419ec
rafadpedrosa <rafadpedrosa@gmail.com>
2018-08-15 01:09:42 -0300

    improoving solution

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -10,16 +10,9 @@ class ArmstrongNumbers {
         numberPorSum = numbersToCheckConverted
                 .chars()
                 .mapToLong(n -> Character.getNumericValue(n))
-                .mapToInt(number -> this.pow(number, numberSize))
+                .mapToInt(number -> (int) Math.pow(number, numberSize))
                 .sum();
 
         return numberPorSum == numberToCheck;
     }
-
-    private int pow(long number, int pow) {
-        Double convertedNumber = Double.valueOf(number);
-        Double convertedPow = Double.valueOf(String.valueOf(pow));
-        return (int) Math.pow(convertedNumber, convertedPow);
-    }
-
 }
