a5a64c70937ceea0c9d4212cadd9ee9aef07b034
rafadpedrosa <rafadpedrosa@gmail.com>
2018-08-21 19:18:56 -0300

    adding a better solution

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -26,3 +26,15 @@ public class PangramChecker {
                 .allMatch(value -> value);
     }
 }
+
+// BETTER SOLUTION
+//public class PangramChecker {
+//    private final static int LETTERS_IN_ALPHABET = 26;
+//
+//    public boolean isPangram(String input) {
+//        return input.toLowerCase().chars()
+//                .filter(Character::isAlphabetic)
+//                .distinct()
+//                .count() == LETTERS_IN_ALPHABET;
+//    }
+//}
